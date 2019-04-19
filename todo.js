33ea30f2c814b74585e4bd3e1de0ef7ad14e090e
Demonyker <36102820+Demonyker@users.noreply.github.com>
2019-04-19 14:49:52 +0300

    Stat

--- todo.js
+++ todo.js
@@ -1,63 +1,39 @@
-var Stroka = [];
-var tags = [];
-// Так как по заданию не было уточнено, подготовил заполненный массив объектов для проверки работы функий
-// Если будет нужно можно будет создать функцию которая заполняет массив объектов(строк)
-Stroka[0] = {
+tags = [];
+Stroka = [
+{
   priority:"A",
   tags:["job", "important", "money"],
   text:"Create a to do list on javascrpit",
-  date: new Date(2019, 3, 24)
-}
-Stroka[1] = {
+  date: new Date(2019, 3, 25)
+},
+{
   priority:"B",
   tags: ["breakfest", "food"],
   text:"Visit a McDonalds",
   date: new Date(2019, 3, 24)
-}
-Stroka[2] = {
+},
+{
   priority:"A",
   tags: ["science", "study", "institute"],
   text:"Do homework",
-  date: new Date(2019, 3, 24)
+  date: new Date(2019, 3, 22)
 }
-Stroka[3] = {
+,{
   priority:"B",
   tags: ["study", "institute"],
   text:"attend a lecture",
-  date: new Date(2019, 3, 25)
+  date: new Date(2019, 3, 23)
 }
-//Создаю класс который будет работать с массивом объектов
+];
+
 class Todo{
-  constructor(priority, tags, text, date){
-    this.priority = priority;
-    this.tags = tags;
-    this.text = text;
-    this.date = date;
-  }
-//Метод фильтрации, в данном примере есть две функции, которые выводят строки либо только с приоритетом А, либо с приоритетом B
-filterByA(Stroks){
-  function filterByP1(Stroks){
-   if (Stroks.priority == "A") {
-    return true;
-  }else
-   return false;
- }
-  var arrByP = Stroks.filter(filterByP1);
-  console.log(arrByP);
+filterByType(arr, typeP){
+return arr.filter(function(arr){
+return arr.priority === typeP
+});
 }
-filterByB(Stroks){
-  function filterByP2(Stroks){
-   if (Stroks.priority == "B") {
-    return true;
-  }else
-   return false;
- }
-  var arrByP = Stroks.filter(filterByP2);
-  console.log(arrByP);
-}
-//Метод сортировки по приоритету(я еще создам сортировку по дате)
-sortirovka(Stroks){
- console.log( Stroks.sort(function (a, b) {
+sortirovkaByP(arr){
+ console.log( arr.sort(function (a, b) {
  if (a.priority > b.priority) {
 return 1;
   }
@@ -68,7 +44,26 @@ return -1;
  return 0;
 }),)
 }
+sortirovkaByD(arr){
+ console.log( arr.sort(function (a, b) {
+ if (a.date > b.date) {
+return 1;
+  }
+  if (a.date < b.date) {
+return -1;
+   }
+
+ return 0;
+}),)
 }
-//Получение классом массива объектов
-var Stroks = new Todo(Stroka.priority, Stroka.tags, Stroka.text, Stroka.date);
-//в html файле этот же код только с проверками методов
+statistick(arr, type, znach){
+  var k = 0;
+  for( i = 0; i < arr.lenght; i++){
+    if(arr[i].type == znach){
+      k = k + 1;
+    }
+  }
+  console.log('Значение ' + type + znach + 'встречается' + k + 'раз')
+}
+}
+var todolist = new Todo();
